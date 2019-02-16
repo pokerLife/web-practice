6a9bb7ed58c19d813c0c53d7ffa8421f5c6f9df6
mr_zhaojie <mr_zhaojie@163.com>
2019-02-16 09:37:14 +0800

    编辑器光标位置

--- web-api/editor/editor.js
+++ web-api/editor/editor.js
@@ -2,6 +2,21 @@ class Editor {
     constructor(editor) {
         this.editor = editor;
     }
+    /**
+     * 光标位置
+     */
+    editRange() {
+        let lastEditRange;
+        // 编辑框点击事件
+        let editor = document.getElementById(this.editor);
+        editor.addEventListener('click', e => {
+            // 获取选定对象
+            let selection = getSelection()
+            console.log(selection)
+            // 设置最后光标对象
+            lastEditRange = selection.getRangeAt(0)
+        })
+    }
     copy() {
         let copy = document.getElementById('copy');
         copy.addEventListener('click', e => {
@@ -55,10 +70,32 @@ class Editor {
         })
     }
     /** 插入水平线 */
-    insertHorizontalRule(){
+    insertHorizontalRule() {
         let btn = document.getElementById('insertHorizontalRule');
         btn.addEventListener('click', e => {
             document.execCommand('insertHorizontalRule');
+        });
+    }
+    images() {
+        let btn = document.getElementById('images');
+        btn.addEventListener('click', e => {
+            let url = prompt('请输入网络图片路径', 'http://img17.3lian.com/d/file/201701/16/779db6efe9d4520e07e8bfb8b9e55175.jpg');
+            document.execCommand('insertImage', false, url);
+        });
+    }
+    /**
+     * 有序列表
+     */
+    insertOrderedList() {
+        let btn = document.getElementById('insertOrderedList');
+        btn.addEventListener('click', e => {
+            document.execCommand('insertOrderedList');
+        });
+    }
+    insertUnorderedList() {
+        let btn = document.getElementById('insertUnorderedList');
+        btn.addEventListener('click', e => {
+            document.execCommand('insertUnorderedList');
         });
     }
     /** 初始化编辑器 */
@@ -71,5 +108,9 @@ class Editor {
         this.underline();
         this.title();
         this.insertHorizontalRule();
+        this.images();
+        this.insertOrderedList();
+        this.insertUnorderedList();
+        this.editRange();
     }
 }
