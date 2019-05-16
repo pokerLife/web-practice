77da29e874bb1f95c33574937e1be167ff4d0703
mr_zhaojie <mr_zhaojie@163.com>
2019-05-16 17:06:11 +0800

    转盘抽奖—520

---
+++ vue/lucky-draw/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <my-button size="lg"></my-button>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import Button from "@/components/Button.vue";
+
+export default {
+  components: {
+    "my-button": Button
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
